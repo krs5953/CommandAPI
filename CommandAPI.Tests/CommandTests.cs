99684cd990f92ccee4dc0425bdb562ad8acc0d81
Kyle Sanders <krs5953@hotmail.com>
2022-09-28 14:29:21 -0500

    Implementation of Unit Tests

---
+++ CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,59 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Act
+            testCommand.Platform = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.Platform);
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Act
+            testCommand.CommandLine = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.CommandLine);
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+    }
+}
